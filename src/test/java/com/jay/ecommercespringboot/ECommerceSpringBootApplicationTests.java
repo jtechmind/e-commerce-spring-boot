67e1c77aba706b3d2600a6930d220e1711913c47
jtech mind <jtechmind@gmail.com>
2024-01-28 20:14:58 +0530

    E-Commerce SpringBoot Application first commit

---
+++ src/test/java/com/jay/ecommercespringboot/ECommerceSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jay.ecommercespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
